616adc6c3de8b4b79c363b83fa4044fe155495dd
Caleb <calebcipra@gmail.com>
2021-07-30 16:17:35 -0400

    [hidden] small fixes on imports

--- __init__.py
+++ __init__.py
@@ -8,12 +8,11 @@ base_path = tmp_global_obj["basepath"]
 cur_path = base_path + 'modules' + os.sep + 'face_recognition' + os.sep + 'libs' + os.sep
 sys.path.append(cur_path)
 
-import face_recognition
 import PIL.Image
 import numpy as np
 # face_recognition import main as face_main
 
-from face_recognition_service import Face_recognition_service
+
 
 current_platform = platform.system()
 
@@ -38,7 +37,7 @@ try:
             result = a.communicate()[0].decode()
             
         else:
-
+            from face_recognition_service import Face_recognition_service
             newFace_recognition_service = Face_recognition_service(known_image, image_to_check)
             result = newFace_recognition_service.booleanComparation()
 
